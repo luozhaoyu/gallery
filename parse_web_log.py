91ed0d4cf113ccd5f2eb596bf141f8cb5e2da320
Zhaoyu Luo <luozhaoyu90@gmail.com>
2014-03-31 18:42:35 +0800

    add pre-grep optimization

--- parse_web_log.py
+++ parse_web_log.py
@@ -9,10 +9,10 @@ import os
 import io
 import re
 import time
-import argparse
+import subprocess
 
 
-def parse_lines(lines, start_time=None):
+def parse_lines(lines, start_time=None, verbose=False):
     """
     Args:
         start_time: parse only log time after the specified time string, such as "1998-08-23 21:12:08"
@@ -50,7 +50,8 @@ def parse_lines(lines, start_time=None):
             else:
                 res.append(d)
         else:
-            print "UNMATCHED LINE:", line
+            if verbose:
+                print "UNMATCHED LINE:", line
     return res
 
 
@@ -73,6 +74,48 @@ def parse_single_log(logfile, start_time=None):
     return res
 
 
+def execute(cmd):
+    result = None
+    try:
+        result = subprocess.check_output(
+                cmd,
+                stderr=subprocess.STDOUT,
+                shell=True)
+    except subprocess.CalledProcessError as e:
+        print e
+        print e.cmd, e.output
+        print cmd
+        raise e
+    return result
+
+
+def parse_single_log_with_pregrep(logfile, grepcmd, start_time=None, tmp_folder=None, force_update=False):
+    """grep that file before parsing it
+    Program would skip grep if the tmp file exists TODO
+
+    Args:
+        tmp_folder, the tmp file would be stored here if it is set
+        force_update, it would grep the logfile no matter whether the tmpfile exists
+    """
+    if not grepcmd:
+        return parse_single_log(logfile, start_time=start_time)
+
+    logfilepath = os.path.abspath(logfile)
+    log_name = os.path.basename(logfilepath)
+    tmp_folderpath = os.path.abspath(tmp_folder if tmp_folder else '.')
+    tmp_filepath = os.path.join(tmp_folderpath, log_name + '.pregrep')
+    if force_update or not os.path.exists(tmp_filepath):
+        cmd = "%s %s > %s" % (grepcmd, logfilepath, tmp_filepath)
+        execute(cmd)
+
+    res = parse_single_log(tmp_filepath, start_time=start_time)
+
+    if not tmp_folder:
+        print 'removing'
+        os.remove(tmp_filepath)
+    return res
+
+
 def get_recursive_files(folder_path):
     """
     Returns:
@@ -86,7 +129,11 @@ def get_recursive_files(folder_path):
     return result
 
 
-def parse_folder(folder, start_time=None):
+def parse_folder(folder, start_time=None, grepcmd=None, tmp_folder=None, force_update=False):
+    """
+    Args:
+        grepcmd, tmp_folder, force_update's default value should be the same with parse_single_log_with_pregrep
+    """
     def get_first_last_line(filepath):
         f = open(filepath, 'rb')
         filesize = os.fstat(f.fileno()).st_size
@@ -108,30 +155,42 @@ def parse_folder(folder, start_time=None):
     # It is very IMPORTANT to parse each log file in order
     # since the other application may find the last row in database to resume its operation
     all_files.sort()
+
     res = []
     for each_file in all_files:
         if start_time:
             first, last = get_first_last_line(each_file)
             last_row = parse_lines([last], start_time)
             if last_row:
-                res.extend(parse_single_log(each_file, start_time=start_time))
+                if grepcmd:
+                    res.extend(parse_single_log_with_pregrep(each_file,
+                        grepcmd=grepcmd, start_time=start_time, tmp_folder=tmp_folder, force_update=force_update))
+                else:
+                    res.extend(parse_single_log(each_file, start_time=start_time))
             else:
                 print 'ignore file:', each_file
         else:
-            res.extend(parse_single_log(each_file))
+            if grepcmd:
+                res.extend(parse_single_log_with_pregrep(each_file,
+                    grepcmd=grepcmd, start_time=start_time, tmp_folder=tmp_folder, force_update=force_update))
+            else:
+                res.extend(parse_single_log(each_file))
     return res
 
 
 def _main(argv):
+    import argparse
     parser = argparse.ArgumentParser(description='parse web log')
     exclusives = parser.add_mutually_exclusive_group()
     exclusives.add_argument('-l', '--log', help='single log file')
     exclusives.add_argument('-f', '--folder', help='folder')
     parser.add_argument('-t', '--time', help='filter out logs after startime such as: 1998-12-11 10:23:37', default=None)
+    parser.add_argument('-g', '--grep', help='pregrep', action='store_true')
+    parser.add_argument('-tmp', '--tmp-folder', help='tmp folder', default='.')
     args = parser.parse_args()
     print args
-    res = parse_folder(args.folder, args.time)
-    print res
+    grepcmd = "grep -P \"stats_click.php|stats_imp.php\""
+    parse_single_log_with_pregrep(args.log, grepcmd=grepcmd, start_time=args.time, tmp_folder=args.tmp_folder)
 
 
 if __name__ == '__main__':
